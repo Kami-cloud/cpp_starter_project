b62859f73cd9ee31128f35e4b5b620e57ab62c78
SauravJ5 <sauj4356@gmail.com>
2020-06-17 20:10:30 +0530

    restructing project

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Teri bhen ki\n";
+    std::cout<< "hello there, my name is ritik\n";
+}
--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Teri bhen ki";
-    std::cout<<"hello there, my name is ritik";
-}
